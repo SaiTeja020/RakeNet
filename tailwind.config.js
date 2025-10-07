06c3d0c73d3e3f4611ed5216beeb6c17a9d7abef
SaiTeja020 <sakamsaiteja@gmail.com>
2025-10-07 16:01:45 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-  "./index.html",
-  "./components/**/*.{ts,tsx}",
-  "./context/**/*.{ts,tsx}",
-  "./pages/**/*.{ts,tsx}",
-  "./App.{ts,tsx}",
-],
+    "./index.html",
+    "./components/**/*.{ts,tsx}",
+    "./context/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./App.{ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
