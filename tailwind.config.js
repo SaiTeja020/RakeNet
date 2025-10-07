c5eb78e25fb5f49018a963a71bf315817c8b09dd
SaiTeja020 <sakamsaiteja@gmail.com>
2025-10-07 16:06:10 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,31 @@
 export default {
   content: [
     "./index.html",
-    "./components/**/*.{ts,tsx}",
-    "./context/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
     "./App.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./context/**/*.{ts,tsx}",
+    "./constants.ts",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sail-blue': '#003366',
+        'sail-orange': '#FF6600',
+        'light-gray': '#f4f7fa',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        body: ['Roboto', 'sans-serif'],
+      },
+    },
   },
+  safelist: [
+    // Add any classes you generate dynamically, for example:
+    'bg-sail-blue',
+    'bg-sail-orange',
+    'text-sail-blue',
+    'text-sail-orange',
+  ],
   plugins: [],
 };
